f76946e65529b2501d9b666fa35f8e36e34d7664
Pablo.ovrf <pashapetrokas@gmail.com>
2023-03-06 01:21:03 +0300

    add ProductItem and list

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
-const ProductList = () => {
-    return (
-        <div>
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
 
+const products =[
+    {id:'1', title:'Джинсы', price:60, description:'Синего цвета, прямые'},
+    {id:'2', title:'Куртка', price:60, description:'Серого цвета, легкая'},
+    {id:'3', title:'Джинсы', price:60, description:'Серого цвета, узкие'},
+    {id:'4', title:'Куртка', price:60, description:'Синего цвета, теплая'},
+    {id:'5', title:'Свитер', price:60, description:'Серого цвета, теплый'},
+    {id:'6', title:'Байка', price:60, description:'Розового цвета, теплая'},
+    {id:'7', title:'Снут', price:60, description:'Черного цвета, шерсть'},
+    {id:'8', title:'Куртка', price:60, description:'Серого цвета, теплая'},
+    {id:'9', title:'Брюки', price:60, description:'Черного цвета, со стрелками'},
+    {id:'10', title:'Ботинки', price:60, description:'Черного цвета, покрытые лаком'},
+]
+
+const getTotalPrice=(items)=>{
+    return items.reduce((acc,item)=>{
+        return  acc +=item.price;
+    },0)
+}
+const ProductList = () => {
+    const [addedItems,setAddedItems]=useState([]);
+    const {tg}=useTelegram();
+   const onAdd = (product) => {
+       const alreadyAdded = addedItems.find(item=>item.id===product.id);
+       let newItems=[];
+
+       if(alreadyAdded){
+           newItems=addedItems.filter(item=>item.id !== product.id);
+       }else{
+           newItems=[...addedItems, product];
+       }
+
+       setAddedItems(newItems);
+
+       if(newItems.length===0){
+           tg.MainButton.hide();
+       }else{
+           tg.MainButton.show();
+           tg.MainButton.setParams({
+                text:`Купить ${getTotalPrice(newItems)}`
+           })
+       }
+
+   }
+
+    return (
+        <div className={'list'}>
+            {products.map(item=>(
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+
+            ))}
+        }
         </div>
     );
 };
