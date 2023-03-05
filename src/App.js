ef26deef92ac35c314a0c5502c9d128bc0c8c27a
Pablo.ovrf <pashapetrokas@gmail.com>
2023-03-05 19:53:45 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import './App.css';
 import {useEffect} from "react";
 
 
-const tg= window.Telegram.WebApp;
-
 function App() {
   useEffect(()=>{
     tg.ready();
   },[])
-    const onClose =()=>{
-      tg.close();
-    }
+
   return (
     <div className="App">
     work
-      <button onClick={onClose}>Close</button>
+
     </div>
   );
 }
