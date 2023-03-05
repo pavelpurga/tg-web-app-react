9accb09e621e202f6da97ebe9379e653f1f1940c
Pablo.ovrf <pashapetrokas@gmail.com>
2023-03-05 21:00:55 +0300

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
-
-const tg= window.Telegram.WebApp;
+import './Header.css'
 
 const Header = () => {
     const {tg,user,onClose}=useTelegram();
